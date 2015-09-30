434f559c00d22f133ece2726643fe5b453f635bd
Riley Jones <rileymjones@gmail.com>
2015-09-30 13:53:58 -0600

    Finished Chapter 1

--- chapter_1/ex1.rb
+++ chapter_1/ex1.rb
@@ -22,7 +22,5 @@ puts 'Riley Jones thinks that ruby is a lovely name.'
 
 Make your script print another line.
             Make your script print only one of the lines.
-
-
                               Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
 
